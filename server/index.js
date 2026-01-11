b894801e0e5c829628a1e87505603fb2cc8991e5
Charan161025 <geethacaharan314@gmail.com>
2026-01-12 01:22:54 +0530

    first commit

--- server/index.js
+++ server/index.js
@@ -1,25 +1,17 @@
 const express = require("express");
 const mongoose = require("mongoose");
-const dotenv = require("dotenv");
-import cors from "cors";
-
-const rateLimit=require("express-rate-limit");
-
+require("dotenv").config();
+const cors = require("cors");
+const rateLimit = require("express-rate-limit");
 
 const app = express();
-const path= require("path");
-
-dotenv.config();
+const path = require("path");
 
 app.use((req, res, next) => {
- 
-
   res.setHeader(
     "Content-Security-Policy",
     "default-src * 'unsafe-inline' 'unsafe-eval' data: blob:;"
   );
-
-  
   next();
 });
 
@@ -31,28 +23,28 @@ app.use(cors({
   credentials: true
 }));
 
-
-
-
 const limiter = rateLimit({
- windowMs: 15 * 60 *1000,
- max: 60,
- skip: (req) => req.method === 'OPTIONS',
- standardHeaders: true,
- legacyHeaders: false,
+  windowMs: 15 * 60 * 1000,
+  max: 60,
+  skip: (req) => req.method === 'OPTIONS',
+  standardHeaders: true,
+  legacyHeaders: false,
   message: "Too many requests from this IP, please try again after 15 minutes"
-})
+});
 app.use(limiter);
-
 
 app.use(express.json());
 const clientBuildPath = path.join(__dirname, "../client/dist");
 app.use(express.static(clientBuildPath));
 
-
 app.use("/api/auth", require("./routes/authRoute"));
-app.get("*index", (req, res) => {res.sendFile(path.join(__dirname, "../client/dist/index.html"))});
 
+app.use((req, res, next) => {
+  if (req.path.startsWith('/api')) {
+    return next();
+  }
+  res.sendFile(path.join(__dirname, '../client/dist/index.html'));
+});
 
 mongoose.connect(process.env.MONGO_URL)
   .then(() => {
@@ -60,8 +52,7 @@ mongoose.connect(process.env.MONGO_URL)
   })
   .catch(err => console.log("Mongo Error:", err));
 
-
-const PORT = process.env.PORT ;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log("Server running on port " + PORT);
 });
